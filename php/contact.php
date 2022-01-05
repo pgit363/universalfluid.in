5d1ed7082bd83a462d8d5fb50c079b109709df8a
pranav32 <pranav@indigitalit.com>
2022-01-05 20:21:36 +0530

    footer updated

---
+++ php/contact.php
@@ -0,0 +1,15 @@
+<?php
+    $name = $_POST['name'];
+    echo $name;
+   // if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  
+    //     //$data = $_REQUEST['name'];
+      
+       
+    //     // if (empty($data)) {
+    //     //     echo "data is empty";
+    //     // } else {
+    //     //     echo $data;
+    //     // }
+    // }
+?>
