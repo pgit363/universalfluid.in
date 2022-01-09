0b61dab583097f91592331baf440f786618f7f77
pranav32 <pranav@indigitalit.com>
2022-01-09 15:56:41 +0530

    simple

---
+++ contact.php
@@ -0,0 +1,15 @@
+<?php
+    $name = $_GET['name'];
+    echo $name;
+   // if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  
+    //     //$data = $_REQUEST['name'];
+      
+       
+    //     // if (empty($data)) {
+    //     //     echo "data is empty";
+    //     // } else {
+    //     //     echo $data;
+    //     // }
+    // }
+?>
